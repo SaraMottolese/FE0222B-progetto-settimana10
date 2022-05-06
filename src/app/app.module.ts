48367d8842fcbb94903301e632ff8fca9097d7a3
SaraMottolese <saramottolese89@yahoo.it>
2022-05-05 17:32:19 -0700

    progetto settimana 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Route } from '@angular/router';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './components/task/task.component';
+import { TaskCompletatiComponent } from './components/task-completati/task-completati.component';
 
+const routes:Route[]=[
+  {
+    path:'',
+    component: TaskComponent
+  },
+  {
+    path: 'task-completati',
+    component: TaskCompletatiComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TaskCompletatiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/task-completati/task-completati.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-completati',
+  templateUrl: './task-completati.component.html',
+  styleUrls: ['./task-completati.component.scss']
+})
+export class TaskCompletatiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
