9b2e24f3c21a366dbd2c485f7cba65539f40c185
SaraMottolese <saramottolese89@yahoo.it>
2022-05-07 18:40:29 -0700

    completato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Route } from '@angular/router';
 import { AppComponent } from './app.component';
-import { TaskComponent } from './components/task/task.component';
-import { TaskCompletatiComponent } from './components/task-completati/task-completati.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { TodoComponent } from './components/todo/todo.component';
+import { CompletedComponent } from './components/completed/completed.component';
+
+
 
 const routes:Route[]=[
   {
     path:'',
-    component: TaskComponent
+    component: TodoComponent
   },
   {
     path: 'task-completati',
-    component: TaskCompletatiComponent
+    component: CompletedComponent
   }
 ]
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent,
-    TaskCompletatiComponent
+    TodoComponent,
+    CompletedComponent,
+    NavbarComponent,
+    TodoComponent,
+    CompletedComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/service/todos.service.ts
@@ -0,0 +1,22 @@
+import { Todo } from "../interface/todo";
+
+let todos:Todo[]=[];
+let todoscompletati: Todo[]=[];
+
+export function aggiungiImpegno(task: Todo){
+  todos.push(task)
+  return todos;
+}
+export function riportaTodo(){
+  return todos;
+}
+
+export function creaCompletati(task : Todo){
+  todoscompletati.push(task);
+}
+
+export function riportaCompletati(){
+  return todoscompletati;
+}
+
+
